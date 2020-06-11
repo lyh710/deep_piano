6cba82212817ce7190e6bb540577bce59def69bf
Yonghui Li <lyh710@gmail.com>
2020-06-11 11:13:20 +1000

    delete ffmpeg.bat with wrong naming

--- ffmpeg_mp3_to-wav.bat
+++
@@ -1,5 +0,0 @@
-:: Win10
-:: FFmpeg: https://ffmpeg.org/download.html#build-windows
-
-SET data_dir=data
-for /r %%i in (%data_dir%\*.mp3) do FFmpeg\bin\ffmpeg -i %%i -acodec pcm_s16le -ac 1 -ar 16000 %data_dir%\%%~ni.wav
