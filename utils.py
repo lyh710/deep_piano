755854469594f10caa2a8422eb9667a288bed8ad
unknown <lyh710@gmail.com>
2021-01-04 16:53:04 +1100

    pitch-duration-offset

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 from music_config import music_config
 import os, shutil, progressbar
 import numpy as np
-from music21 import converter, instrument, note, chord, stream
+from music21 import converter, instrument, note, chord, stream, duration
 
 sr          = music_config['sr']
 win         = music_config['win']
@@ -24,11 +24,13 @@ def show_config(music_config):
     for key in music_config:
         print(key, ': ', music_config[key])
 
-def midi_is_piano(midi_fn):
+def midi_is_valid(midi_fn):
     '''
-    return if the midi has Piano being one of the instrument partition
+    return True if:
+    - The midi can be parsed by music21 package
+    - And Piano being one of the instrument partition, or the midi has notes in a flat structure
     '''
-    isPiano = True
+    isValid = True
     
     try:
         midi = converter.parse(midi_fn)
@@ -36,26 +38,26 @@ def midi_is_piano(midi_fn):
         try: # file has instrument parts
             s2 = instrument.partitionByInstrument(midi)
             if 'PIANO' not in [str(s.getInstrument()).upper() for s in s2]:
-                isPiano = False
+                isValid = False
         except: # file has notes in a flat structure
-            isPiano = True
+            isValid = True
 
     except:
-        isPiano = False
+        isValid = False
 
-    return isPiano
+    return isValid
 
 def split_midi_train_test(midi_dir):
     '''
     Input:
-    - midi_dir: midi files location
+    - midi_dir: downloaded midi files
     Output:
     - a folder named "train", which contains all except one midi file
     - a folder named "test", which contains only one random selected midi file
-    - all the "train" and "test" midi files should contain 'PIANO' or being a flat structure
+    - all the "train" and "test" midi files should be "valid"
     '''
 
-    # cache all the midi to a folder named "piano" if there is piano partition in the midi
+    # cache all the midi to a folder named "piano" if valid
     piano_dir = os.path.join(midi_dir, 'piano')
     if os.path.isdir(piano_dir):
         shutil.rmtree(piano_dir)
@@ -66,7 +68,7 @@ def split_midi_train_test(midi_dir):
     i = 0
     with progressbar.ProgressBar(max_value=len(os.listdir(midi_dir))) as bar:
         for fn in os.listdir(midi_dir):
-            if fn.endswith('.mid') and midi_is_piano(os.path.join(midi_dir,fn)):
+            if fn.endswith('.mid') and midi_is_valid(os.path.join(midi_dir,fn)):
                 src_dir = os.path.join(midi_dir,fn)
                 dst_dir = os.path.join(piano_dir, fn)
                 shutil.move(src_dir, dst_dir)
@@ -108,12 +110,11 @@ def split_midi_train_test(midi_dir):
 def midi_2_notes(midi_fn):
     '''
     - midi_fn: full path to the midi music file
-    - return notes, which is a list of all the elements of all the compositions:
-        - If we process a note, we will store in the list a string representing the pitch (the note name) and the octave.
-        - If we process a chord (Remember that chords are set of notes that are played at the same time) we will store 
-          a different type of string with numbers separated by dots. Each number represents the pitch of a chord note.
-        - We are not considering yet time offsets of each element. So all the notes and chords will have the same duration. 
+    - return below format:
+        - if single note: pitch+duration+offset
+        - if single cord: note*note*note*chord_offset (note in above format) 
     '''
+    
     notes = []
 
     midi = converter.parse(midi_fn)
@@ -131,47 +132,83 @@ def midi_2_notes(midi_fn):
 
     for element in notes_to_parse:
         if isinstance(element, note.Note):
-            notes.append(str(element.pitch))
+            single_note = '+'.join([str(element.pitch), str(element.duration.quarterLength), str(element.offset)])
+            notes.append(single_note)
         elif isinstance(element, chord.Chord):
-            notes.append('.'.join(str(n) for n in element.normalOrder))
+            single_cord = '*'.join(['+'.join([str(n.pitch), str(n.duration.quarterLength), str(n.offset)]) for n in element.notes])
+            single_cord += '*'
+            single_cord += str(element.offset)
+            notes.append(single_cord)
 
     # return notes
     # assemble the list of notes into text-like format string, for subsequent text-based DNN process
     return ' '.join(notes)
 
+def convert_to_float(frac_str):
+    try:
+        return float(frac_str)
+    except ValueError:
+        num, denom = frac_str.split('/')
+        try:
+            leading, num = num.split(' ')
+            whole = float(leading)
+        except ValueError:
+            whole = 0
+        frac = float(num) / float(denom)
+        return whole - frac if whole < 0 else whole + frac
+
+def pattern_2_note(note_pattern):
+    '''
+    pattern = pitch+duration+offset
+    '''
+    element = note_pattern.split('+')
+
+    new_note = note.Note(element[0])
+    new_note.storedInstrument = instrument.Piano()
+
+    d = duration.Duration()
+    d.quarterLength = convert_to_float(str(element[1]))
+    new_note.duration = d
+
+    new_note.offset = convert_to_float(str(element[2]))
+
+    return new_note
+
+def pattern_2_chord(chord_pattern):
+    '''
+    pattern = note*note*note*offset
+    '''
+    note_patterns = chord_pattern.split('*')[:-1]
+    chord_offset  = chord_pattern.split('*')[-1]
+    new_notes = []
+    for note_pattern in note_patterns:
+        new_notes.append(pattern_2_note(note_pattern))
+
+    new_chord = chord.Chord(new_notes)
+    new_chord.offset = convert_to_float(str(chord_offset))
+    return new_chord
+
 def notes_2_midi(prediction_output, midi_fn):
     '''
-    prediction_output: list of notes generated
+    prediction_output: list of notes generated, in below format:
+        - if single note: pitch+duration+offset
+        - if single cord: note*note*note (note in above format) 
     midi_fn          : absolute path of the midi file to write to
     output the notes to the midi_fn
     '''
-    offset = 0
     output_notes = []
     
     # notes = prediction_output
-    notes = prediction_output.split(' ')
+    patterns = prediction_output.split(' ')
 
-    # create note and chord objects based on the values generated by the model
-    for pattern in notes:
+    # create single_note and single_cord objects based on the values generated by the model
+    for pattern in patterns:
         # pattern is a chord
-        if ('.' in pattern) or pattern.isdigit():
-            notes_in_chord = pattern.split('.')
-            notes = []
-            for current_note in notes_in_chord:
-                new_note = note.Note(int(current_note))
-                new_note.storedInstrument = instrument.Piano()
-                notes.append(new_note)
-            new_chord = chord.Chord(notes)
-            new_chord.offset = offset
-            output_notes.append(new_chord)
+        if ('*' in pattern):
+            output_notes.append(pattern_2_chord(pattern))
         # pattern is a note
         else:
-            new_note = note.Note(pattern)
-            new_note.offset = offset
-            new_note.storedInstrument = instrument.Piano()
-            output_notes.append(new_note)
-        # increase offset each iteration so that notes do not stack
-        offset += 0.5
+            output_notes.append(pattern_2_note(pattern))
     
     midi_stream = stream.Stream(output_notes)
     
@@ -199,3 +236,28 @@ def midi_2_csv(train_dir, out_fn):
                 bar.update(i)
                 i += 1
 
+def df_split(df, split = 0.1):
+    '''
+    Split to 1-split : split
+    '''
+    if 'split' in df.columns:
+        df.drop('split', axis=1, inplace=True)
+    
+    df['split'] = 'train'
+    df.loc[np.random.choice(df[df['split']=='train'].index, int(df.shape[0]*valid_split)), 'split'] = 'valid'
+    
+    return df
+
+def pkl_dump(data, fn):
+    if os.path.exists(fn):
+        os.remove(fn)
+    with open(fn, 'wb') as f:
+        pickle.dump(data, f, protocol=4)
+        
+def pkl_load(fn):
+    if not os.path.exists(fn):
+        print('data not available, check')
+        exit()
+    else:
+        with open(fn, 'rb') as f:
+            return pickle.load(f)
