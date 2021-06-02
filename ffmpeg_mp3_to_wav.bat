bbeacf828d28301fa5f3a263c1a1786f39eda0c3
unknown <lyh710@gmail.com>
2021-06-02 19:22:04 +1000

    clean master

--- ffmpeg_mp3_to_wav.bat
+++
@@ -1,6 +0,0 @@
-:: Win10
-:: FFmpeg: https://ffmpeg.org/download.html#build-windows
-
-SET data_dir=%1
-
-for %%f in (%data_dir%\*.mp3) do FFmpeg\bin\ffmpeg -i %%f -acodec pcm_s16le -ac 1 -ar 16000 %data_dir%\%%~nf.wav
